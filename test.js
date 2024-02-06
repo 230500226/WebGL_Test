99721c5ecb1aca3e4a9fdb5e12958867472d0fdc
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2024-02-07 00:58:31 +0200

    v2

--- test.js
+++ test.js
@@ -32,8 +32,8 @@ gl.shaderSource(vertexShader, vertexShaderSourceCode);
 gl.compileShader(vertexShader);
 
 if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
-    const compileError = gl.getShaderInfoLog(vertexShader);
-    showError('Compile vertex error: ' + compileError);
+    const errorMessage = gl.getShaderInfoLog(vertexShader);
+    showError('Compile vertex error: ' + errorMessage);
     return;
 }
 
@@ -50,8 +50,8 @@ gl.shaderSource(fragmentShader, fragmentShaderSourceCode);
 gl.compileShader(fragmentShader);
 
 if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
-    const compileError = gl.getShaderInfoLog(fragmentShader);
-    showError('Compile fragment error: ' + compileError);
+    const errorMessage = gl.getShaderInfoLog(fragmentShader);
+    showError('Compile fragment error: ' + errorMessage);
     return;
 }
 
@@ -60,8 +60,17 @@ gl.attachShader(program, vertexShader);
 gl.attachShader(program, fragmentShader);
 gl.linkProgram(program);
 
-const positionLocation = gl.getAttribLocation(program, "vertexPosition");
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    const errorMessage = gl.getProgramInfoLog(helloTriangleProgram);
+    showError(`Failed to link GPU program: ${errorMessage}`);
+    return;
+  }
 
+const squarePosition = gl.getAttribLocation(program, "vertexPosition");
+ if (squarePosition < 0) {
+    showError(`Failed to get attribute location for vertexPosition`);
+    return;
+  }
 const buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
@@ -71,7 +80,8 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
     -0.8,  0.8,
     0.8, -0.8,
     0.8,  0.8]), gl.STATIC_DRAW);
-
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
 gl.clearColor(0, 0, 0, 1);
 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -79,6 +89,6 @@ gl.enableVertexAttribArray(squarePosition);
 
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 
-gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+gl.vertexAttribPointer(squarePosition, 2, gl.FLOAT, false, 0, 0);
 
 gl.drawArrays(gl.TRIANGLES, 0, 6);
